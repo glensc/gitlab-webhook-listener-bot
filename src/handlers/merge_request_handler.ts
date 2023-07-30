41c693edc9e8f9c305c6d1e75b58d3bb95c0302d
Elan Ruusamäe <glen@delfi.ee>
2023-07-30 23:17:12 +0300

    Implement merge request handler

--- src/handlers/merge_request_handler.ts
+++ src/handlers/merge_request_handler.ts
@@ -5,6 +5,30 @@ import { MergeRequestPayload } from "./types";
 export class MergeRequestHandler extends BaseHandler implements Handler {
   public event_type = "merge_request";
 
-  public async handle(payload: MergeRequestPayload): Promise<void> {
+  public async handle({
+                        object_attributes: {
+                          source_branch,
+                          action,
+                          state,
+                        },
+                        changes: {
+                          description: {
+                            current: description
+                          },
+                        },
+                      }: MergeRequestPayload): Promise<void> {
+
+    // Must be an opened mr whose status is updated
+    // and branch is renovate branch
+    // and rebase checkbox is checked.
+    if (
+      state !== "opened" ||
+      action !== "update" ||
+      !source_branch.startsWith("renovate/") ||
+      !description.includes('[x] <!-- rebase-check -->')) {
+      return;
+    }
+
+    this.logger.debug("Renovate bot wants rebase");
   }
 }
