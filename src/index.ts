3fec25eb537a2c9fc96d3201284eba1167dcf696
Elan Ruusamäe <glen@delfi.ee>
2023-07-31 00:28:21 +0300

    Export main and logger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { main as default } from "./entrypoint";
+export { main } from "./entrypoint";
+export { default as logger } from "./services/logger";
 
 // Reusable classes
 export { MergeRequestHandler } from "./handlers/merge_request_handler";
