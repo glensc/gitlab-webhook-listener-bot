75ffe5cc211a5896ed93fcfca9e2a19789cfe52b
Elan Ruusamäe <glen@delfi.ee>
2023-07-30 22:09:49 +0300

    Register webhook listener

--- src/services/routes/index.ts
+++ src/services/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import main from "./main";
+import webhook from "./webhook";
 
 const router = Router();
 
 router.get("/", main);
+router.post("/webhook", webhook);
 
 export default router;
