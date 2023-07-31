c2fcd675ff50945a09786118ff767d6a1fbcb3ed
Elan Ruusamäe <glen@delfi.ee>
2023-07-31 11:06:04 +0300

    Export BaseHandler, Handler

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export { main } from "./entrypoint";
 export { default as logger } from "./services/logger";
 
 // Reusable classes
+export { BaseHandler } from "./handlers/base_handler";
+export { Handler } from "./handlers/handler"
 export { MergeRequestHandler } from "./handlers/merge_request_handler";
 export { MergeRequestPayload } from "./handlers/types";
