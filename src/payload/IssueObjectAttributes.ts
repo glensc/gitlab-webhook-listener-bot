e2aeffa7da4395da2b83cb4e361bab817db235eb
Elan Ruusamäe <glen@delfi.ee>
2023-08-20 19:00:14 +0300

    Create ObjectAttributes abstraction

--- src/payload/IssueObjectAttributes.ts
+++ src/payload/IssueObjectAttributes.ts
@@ -1,38 +1,15 @@
 import { Label } from "./Label";
+import { ObjectAttributes } from "./ObjectAttributes";
 
-export interface IssueObjectAttributes {
-  author_id: number;
+export interface IssueObjectAttributes extends ObjectAttributes {
   closed_at: string;
   confidential: boolean;
-  created_at: string;
-  description: string;
   discussion_locked: true | null;
   due_date: string | null;
-  id: number;
-  iid: number;
-  last_edited_at: string;
-  last_edited_by_id: number;
-  milestone_id: number | null;
   moved_to_id: number | null;
   duplicated_to_id: number | null;
   project_id: number;
   relative_position: number;
-  state_id: number;
-  time_estimate: number;
-  title: string;
-  updated_at: string;
-  updated_by_id: number;
   weight: number | null;
-  url: string;
-  total_time_spent: number;
-  time_change: number;
-  human_total_time_spent: string | null;
-  human_time_change: string | null;
-  human_time_estimate: string| null;
-  assignee_ids: number[] | null;
-  assignee_id: number;
-  labels: Label[] | null;
-  state: string;
   severity: string;
-  action: string;
 }
--- src/payload/MergeRequestObjectAttributes.ts
+++ src/payload/MergeRequestObjectAttributes.ts
@@ -1,17 +1,9 @@
 import { Project } from "./Project";
-import { Label } from "./Label";
 import { Author } from "./Author";
+import { ObjectAttributes } from "./ObjectAttributes";
 
-export interface MergeRequestObjectAttributes {
-  assignee_id: number;
-  author_id: number;
-  created_at: string;
-  description: string;
+export interface MergeRequestObjectAttributes extends ObjectAttributes {
   head_pipeline_id: number | null;
-  id: number;
-  iid: number;
-  last_edited_at: string | null;
-  last_edited_by_id: number | null;
   merge_commit_sha: string | null;
   merge_error: string | null;
   merge_params: {
@@ -20,17 +12,10 @@ export interface MergeRequestObjectAttributes {
   merge_status: string;
   merge_user_id: string | null;
   merge_when_pipeline_succeeds: boolean;
-  milestone_id: string| null;
   source_branch: string;
   source_project_id: number;
-  state_id: number;
   target_branch: string;
   target_project_id: number;
-  time_estimate: number;
-  title: string;
-  updated_at: string;
-  updated_by_id: number | null;
-  url: string;
   source: Project;
   target: Project;
   last_commit: {
@@ -42,17 +27,8 @@ export interface MergeRequestObjectAttributes {
     author: Author;
   };
   work_in_progress: boolean;
-  total_time_spent: number;
-  time_change: number;
-  human_total_time_spent: string | null;
-  human_time_change: string | null;
-  human_time_estimate: string | null;
-  assignee_ids: number[] | null;
   reviewer_ids: number[] | null;
-  labels: Label[] | null;
-  state: string;
   blocking_discussions_resolved: boolean;
   first_contribution: boolean;
   detailed_merge_status: string;
-  action: string;
 }
