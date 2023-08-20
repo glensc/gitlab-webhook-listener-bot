b89a8b04b443f5fb775ebc802bd016faab4832de
Elan Ruusamäe <glen@delfi.ee>
2023-08-20 19:26:24 +0300

    Update MergeStatus from source code

--- src/payload/Changes.ts
+++ src/payload/Changes.ts
@@ -5,14 +5,22 @@ type PreviousAndCurrent<T> = {
   current: T;
 };
 
-type MergeStatus = "unchecked" | "preparing" | "can_be_merged";
+// https://gitlab.com/gitlab-org/gitlab/-/blob/171e6985759885fd7acbe28972fc97f5c9c81c46/spec/graphql/types/merge_request_type_spec.rb#L125
+type MergeStatus =
+  "preparing" |
+  "unchecked" |
+  "cannot_be_merged_recheck" |
+  "checking" |
+  "cannot_be_merged_rechecking" |
+  "can_be_merged" |
+  "cannot_be_merged";
 
 export interface Changes {
   description?: PreviousAndCurrent<string>;
   labels?: PreviousAndCurrent<Label[]>;
   last_edited_at?: PreviousAndCurrent<string>;
   last_edited_by_id?: PreviousAndCurrent<number>;
-  merge_status?: PreviousAndCurrent<MergeStatus | string>;
+  merge_status?: PreviousAndCurrent<MergeStatus>;
   state_id?: PreviousAndCurrent<number>;
   target_branch?: PreviousAndCurrent<string>;
   title?: PreviousAndCurrent<string>;
