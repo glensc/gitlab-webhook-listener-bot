0764b20359ba74db1ea66a0a98a8c208997599cf
Elan Ruusamäe <glen@delfi.ee>
2023-08-21 20:57:21 +0300

    Refactor to use EVENT_TYPES enum

--- src/handlers/DeploymentHandler.ts
+++ src/handlers/DeploymentHandler.ts
@@ -1,10 +1,10 @@
 import { BaseHandler } from "./BaseHandler";
 import { Handler } from "./Handler";
 import { DeploymentPayload } from "../payload";
-import { Event } from "../types";
+import { Event, EVENT_TYPES } from "../types";
 
 export abstract class DeploymentHandler<P = DeploymentPayload> extends BaseHandler implements Handler {
-  public event_types = ["deployment"];
+  public event_types = [EVENT_TYPES.DEPLOYMENT];
   public abstract isValid(event: Event<P>): boolean;
   public abstract handle(event: Event<P>): Promise<void>;
 }
--- src/handlers/MergeRequestHandler.ts
+++ src/handlers/MergeRequestHandler.ts
@@ -1,10 +1,10 @@
 import { BaseHandler } from "./BaseHandler";
 import { Handler } from "./Handler";
 import { MergeRequestPayload } from "../payload";
-import { Event } from "../types";
+import { Event, EVENT_TYPES } from "../types";
 
 export abstract class MergeRequestHandler<P = MergeRequestPayload> extends BaseHandler implements Handler {
-  public event_types = ["merge_request"];
+  public event_types = [EVENT_TYPES.MERGE_REQUEST];
   public abstract isValid(event: Event<P>): boolean;
   public abstract handle(event: Event<P>): Promise<void>;
 }
