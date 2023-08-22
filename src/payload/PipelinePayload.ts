553d673e4583fe0d9faf041ce325f7288d567a34
Elan Ruusamäe <glen@delfi.ee>
2023-08-22 16:11:08 +0300

    Use fixed object_kind for PipelinePayload

--- src/payload/PipelinePayload.ts
+++ src/payload/PipelinePayload.ts
@@ -6,7 +6,7 @@ import { User } from "./User";
 
 // https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html#pipeline-events
 export interface PipelinePayload {
-  object_kind: string;
+  object_kind: "pipeline";
   object_attributes: PipelineObjectAttributes;
   // TODO merge_request
   merge_request: any | null;
