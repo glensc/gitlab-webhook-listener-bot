2c6f4dcc83837dc803afe3692fcdd2553ddaff88
Elan Ruusamäe <glen@delfi.ee>
2023-08-22 07:54:15 +0300

    Cleanup unused Event import

--- src/handlers/DeploymentHandler.ts
+++ src/handlers/DeploymentHandler.ts
@@ -1,7 +1,7 @@
 import { BaseHandler } from "../core/BaseHandler";
 import { Handler } from "../core/Handler";
 import { DeploymentPayload } from "../payload";
-import { Event, EVENT_TYPES } from "../types";
+import { EVENT_TYPES } from "../types";
 
 export abstract class DeploymentHandler<P = DeploymentPayload> extends BaseHandler<P> implements Handler {
   public event_types = [EVENT_TYPES.DEPLOYMENT];
--- src/handlers/MergeRequestHandler.ts
+++ src/handlers/MergeRequestHandler.ts
@@ -1,7 +1,7 @@
 import { BaseHandler } from "../core/BaseHandler";
 import { Handler } from "../core/Handler";
 import { MergeRequestPayload } from "../payload";
-import { Event, EVENT_TYPES } from "../types";
+import { EVENT_TYPES } from "../types";
 
 export abstract class MergeRequestHandler<P = MergeRequestPayload> extends BaseHandler<P> implements Handler {
   public event_types = [EVENT_TYPES.MERGE_REQUEST];
