18843b13b22feac34f5c065c80504cfe2fff33aa
Elan Ruusamäe <glen@delfi.ee>
2023-08-22 17:32:01 +0300

    Add JobHandler class

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,3 +1,4 @@
 export { DeploymentHandler } from "./DeploymentHandler";
+export { JobHandler } from "./JobHandler";
 export { MergeRequestHandler } from "./MergeRequestHandler";
 export { PipelineHandler } from "./PipelineHandler";
