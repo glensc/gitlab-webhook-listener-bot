7f63101c18ebb321b44c04df6e6d6ad661f81edc
Elan Ruusamäe <glen@delfi.ee>
2023-09-27 01:10:36 +0300

    Execute livenessProbe, readinessProbe in probe handlers

--- src/routes/probes.ts
+++ src/routes/probes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import registry from "../services/registry";
 
 const router = Router();
 
@@ -7,12 +8,24 @@ const router = Router();
 // liveness probes could catch a deadlock, where an application is running, but unable to make progress.
 // Restarting a container in such a state can help to make the application more available despite bugs.
 router.get("/liveness", (req, res) => {
-  res.status(200).send("ok\n");
+  try {
+    registry.livenessProbe();
+    res.status(200).send("ok\n");
+  } catch (err) {
+    registry.logger.error(err);
+    res.status(500).send("err\n");
+  }
 });
 
 // pod is ready to accept traffic
 router.get("/readiness", (req, res) => {
-  res.status(200).send("ok\n");
+  try {
+    registry.readinessProbe();
+    res.status(200).send("ok\n");
+  } catch (err) {
+    registry.logger.error(err);
+    res.status(500).send("err\n");
+  }
 });
 
 export default router;
