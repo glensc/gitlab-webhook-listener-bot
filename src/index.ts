3bc9049881e31ec73c5b8c5788e30e7b0346977c
Elan Ruusamäe <glen@delfi.ee>
2024-02-20 17:41:07 +0200

    Export gitlab module

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ export type { LoggerInterface } from "./services/logger";
 export type { WebhookEvent, Probe } from "./types";
 export { EVENT_TYPES } from "./types";
 export { BaseHandler } from "./core/BaseHandler";
+export * from "./gitlab";
 export * from "./handlers";
 export * from "./payload";
