b2023d0758fdb776b6ef6b0a481c1ce306a970a5
Elan Ruusamäe <glen@delfi.ee>
2024-03-16 17:17:26 +0200

    Use shared MergeStatus type

--- src/payload/Changes.ts
+++ src/payload/Changes.ts
@@ -1,20 +1,11 @@
 import { Label } from "./Label";
 import { DateTime } from "./types";
+import { MergeStatus } from "./merge_request/types";
 
 type PreviousAndCurrent<T> = {
   previous: T;
   current: T;
 };
-
-// https://gitlab.com/gitlab-org/gitlab/-/blob/171e6985759885fd7acbe28972fc97f5c9c81c46/spec/graphql/types/merge_request_type_spec.rb#L125
-export type MergeStatus =
-  | "can_be_merged"
-  | "cannot_be_merged"
-  | "cannot_be_merged_recheck"
-  | "cannot_be_merged_rechecking"
-  | "checking"
-  | "preparing"
-  | "unchecked";
 
 export interface Changes {
   closed_at?: PreviousAndCurrent<DateTime>;
