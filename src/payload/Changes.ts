5e0c5de64f00dea5b22ad05bf2066a006a03b95e
Elan Ruusamäe <glen@delfi.ee>
2024-03-16 17:16:26 +0200

    Extract MergeStatus type

--- src/payload/Changes.ts
+++ src/payload/Changes.ts
@@ -7,14 +7,14 @@ type PreviousAndCurrent<T> = {
 };
 
 // https://gitlab.com/gitlab-org/gitlab/-/blob/171e6985759885fd7acbe28972fc97f5c9c81c46/spec/graphql/types/merge_request_type_spec.rb#L125
-type MergeStatus =
-  "preparing" |
-  "unchecked" |
-  "cannot_be_merged_recheck" |
-  "checking" |
-  "cannot_be_merged_rechecking" |
-  "can_be_merged" |
-  "cannot_be_merged";
+export type MergeStatus =
+  | "can_be_merged"
+  | "cannot_be_merged"
+  | "cannot_be_merged_recheck"
+  | "cannot_be_merged_rechecking"
+  | "checking"
+  | "preparing"
+  | "unchecked";
 
 export interface Changes {
   closed_at?: PreviousAndCurrent<DateTime>;
