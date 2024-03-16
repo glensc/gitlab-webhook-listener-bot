f8c63be774e04027f8aa7f4a56cc4f06900738af
Elan Ruusamäe <glen@delfi.ee>
2024-03-16 19:46:56 +0200

    Export MergeParams type

--- src/payload/merge_request/types.ts
+++ src/payload/merge_request/types.ts
@@ -7,3 +7,12 @@ export type MergeStatus =
   | "checking"
   | "preparing"
   | "unchecked";
+
+export type MergeParams = {
+  force_remove_source_branch: "0" | "1" | true;
+  auto_merge_strategy?: string;
+  should_remove_source_branch?: boolean;
+  commit_message?: string;
+  squash_commit_message?: string;
+  sha?: string;
+};
