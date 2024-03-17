ec6c100435c747042ff649bd9e986129e3065cc8
Elan Ruusamäe <glen@delfi.ee>
2024-03-17 04:39:51 +0200

    Add relative_position to Changes

--- src/payload/Changes.ts
+++ src/payload/Changes.ts
@@ -22,6 +22,7 @@ export interface Changes {
   merge_status?: PreviousAndCurrent<MergeStatus>;
   merge_user_id?: PreviousAndCurrent<number | null>;
   merge_when_pipeline_succeeds?: PreviousAndCurrent<boolean>;
+  relative_position?: PreviousAndCurrent<number>;
   reviewers?: PreviousAndCurrent<User[]>;
   severity?: PreviousAndCurrent<string>;
   state_id?: PreviousAndCurrent<number>;
