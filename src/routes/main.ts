dbd6eb0a1c039075a36691f17980fb42f7bb7062
Elan Ruusamäe <glen@delfi.ee>
2024-03-17 14:05:05 +0200

    Update main route not being async

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 
-export default async (req: Request, res: Response) => {
+export default (req: Request, res: Response) => {
   res.contentType("text/plain");
   res.send("Gitlab WebHook Listener Bot - https://github.com/glensc/gitlab-webhook-listener-bot");
 };
