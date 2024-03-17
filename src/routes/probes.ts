e8aeaa7b3490fb4ebc46a29f0eeb2aa0c00a9e16
Elan Ruusamäe <glen@delfi.ee>
2024-03-17 13:48:25 +0200

    Re-format route handlers with prettier

--- src/routes/probes.ts
+++ src/routes/probes.ts
@@ -9,25 +9,31 @@ const router: Router = Router();
 
 // liveness probes could catch a deadlock, where an application is running, but unable to make progress.
 // Restarting a container in such a state can help to make the application more available despite bugs.
-router.get("/liveness", asyncHandler(async (req, res) => {
-  try {
-    await registry.livenessProbe();
-    res.status(200).send("ok\n");
-  } catch (err) {
-    registry.logger.error(err);
-    res.status(500).send("err\n");
-  }
-}));
+router.get(
+  "/liveness",
+  asyncHandler(async (req, res) => {
+    try {
+      await registry.livenessProbe();
+      res.status(200).send("ok\n");
+    } catch (err) {
+      registry.logger.error(err);
+      res.status(500).send("err\n");
+    }
+  }),
+);
 
 // pod is ready to accept traffic
-router.get("/readiness", asyncHandler(async (req, res) => {
-  try {
-    await registry.readinessProbe();
-    res.status(200).send("ok\n");
-  } catch (err) {
-    registry.logger.error(err);
-    res.status(500).send("err\n");
-  }
-}));
+router.get(
+  "/readiness",
+  asyncHandler(async (req, res) => {
+    try {
+      await registry.readinessProbe();
+      res.status(200).send("ok\n");
+    } catch (err) {
+      registry.logger.error(err);
+      res.status(500).send("err\n");
+    }
+  }),
+);
 
 export default router;
